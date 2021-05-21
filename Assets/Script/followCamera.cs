cd0de37287f3d3e41a75d108109b94e13284bc3d
sasc0076 <sasc0076@users.noreply.github.com>
2021-05-21 12:45:32 +0200

    NewProjekt
    lets start dis shit again

---
+++ Assets/Script/followCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followCamera : MonoBehaviour
+{
+    public Transform trans_camera;
+    Vector3 newpos;
+    public float offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        newpos =new Vector3(trans_camera.position.x, trans_camera.position.y-offset, trans_camera.position.z);
+        this.transform.position = newpos;
+    }
+}
