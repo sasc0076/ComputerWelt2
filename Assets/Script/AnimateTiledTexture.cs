a37f332f49613c9aecae17db15a3f80c75dfc900
sasc0076 <sasc0076@users.noreply.github.com>
2021-06-17 16:49:42 +0200

    failed clippy animation UGHHHH FUCK DEN SHIT
    ich probiers wann anders nochmal :))))))))))

--- Assets/Script/AnimateTiledTexture.cs
+++ Assets/Script/AnimateTiledTexture.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class AnimateTiledTexture : MonoBehaviour
 {
-    public int columns = 2;
-    public int rows = 2;
+    public int columns;
+    public int rows;
     public float framesPerSecond = 10f;
+    public bool reset;
 
     //the current frame to display
     private int index = 0;
@@ -15,9 +16,6 @@ public class AnimateTiledTexture : MonoBehaviour
     {
         StartCoroutine(updateTiling());
 
-        //set the tile size of the texture (in UV units), based on the rows and columns
-        Vector2 size = new Vector2(1f / columns, 1f / rows);
-        GetComponent<Renderer>().sharedMaterial.SetTextureScale("_MainTex", size);
     }
 
     private IEnumerator updateTiling()
@@ -26,9 +24,15 @@ public class AnimateTiledTexture : MonoBehaviour
         {
             //move to the next index
             index++;
-            if (index >= rows * columns)
+            if (index >= rows * columns || reset)
                 index = 0;
+            /*   if (reset)
+               {
+                   index = 0;
+                   reset = !reset;
+               } */
 
+    
             //split into x and y indexes
             Vector2 offset = new Vector2((float)index / columns - (index / columns), //x index
                                           (index / columns) / (float)rows);          //y index
@@ -36,6 +40,11 @@ public class AnimateTiledTexture : MonoBehaviour
             GetComponent<Renderer>().sharedMaterial.SetTextureOffset("_MainTex", offset);
 
             yield return new WaitForSeconds(1f / framesPerSecond);
+
+
+            //set the tile size of the texture (in UV units), based on the rows and columns
+            Vector2 size = new Vector2(1f / columns, 1f / rows);
+            GetComponent<Renderer>().sharedMaterial.SetTextureScale("_MainTex", size);
         }
 
     }
