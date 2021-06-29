e6a743b338e3e952c81f9cdca48caa3c480b3700
sasc0076 <sasc0076@users.noreply.github.com>
2021-06-29 14:23:20 +0200

    Cursor, Zeitdisplay, neue Font

---
+++ Assets/Script/DisplayTime.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DisplayTime : MonoBehaviour
+{
+    public GameObject theDisplay;
+    int hour;
+    int minutes;
+    // Start is called before the first frame update
+    void Start()
+    {
+      /*  hour = System.DateTime.Now.Hour;
+        minutes = System.DateTime.Now.Minute;
+        theDisplay.GetComponent<TextMeshPro>().text = "" + hour + ":" + minutes;
+      */
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hour = System.DateTime.Now.Hour;
+        minutes = System.DateTime.Now.Minute;
+        if(minutes < 10)
+        {
+            theDisplay.GetComponent<TextMeshPro>().text = "" + hour + ":0" + minutes;
+        }
+        else theDisplay.GetComponent<TextMeshPro>().text = "" + hour + ":" + minutes;
+    }
+}
