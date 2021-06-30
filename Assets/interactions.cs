a7be005af679149083a1bf54d795d856d02ee547
sasc0076 <sasc0076@users.noreply.github.com>
2021-07-01 01:38:08 +0200

    stuff30.06

---
+++ Assets/interactions.cs
@@ -0,0 +1,40 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class interactions : MonoBehaviour
+{
+    public Renderer texture;
+    public Material newmaterial, currentmaterial;
+
+
+    private void Start()
+    {
+        currentmaterial = texture.material;
+    }
+    public void SelectTexture(bool active)
+    {
+        if (active)
+        {
+            texture.material = newmaterial;
+        }
+      
+        if (!active)
+        {
+            texture.material = currentmaterial;
+        }
+
+    }
+
+    public void BoxAppear(GameObject box)
+    {
+        box.SetActive(true);
+    }
+
+    public void BoxDisappear(GameObject box)
+    {
+        box.SetActive(false);
+
+    }
+}
